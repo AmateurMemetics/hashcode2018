c5da4a67e6245e8470c5bc37b0b2ca2207fa758a
Sam Lord <Sam Lord>
2018-03-01 19:35:48 +0000

    Added some funcs

--- hashcode2018.py
+++ hashcode2018.py
@@ -12,8 +12,27 @@ def evalVRPTW(individual, instance, bonus=0):
     totalCost = 0
     route = ind2route(individual, instance)
     
+def cxPartiallyMatched(ind1, ind2):
+    size = min(len(ind1), len(ind2))
+    cxpoint1, cxpoint2 = sorted(random.sample(range(size), 2))
+    temp1 = ind1[cxpoint1:cxpoint2+1] + ind2
+    temp2 = ind1[cxpoint1:cxpoint2+1] + ind2
+    ind1 = []
+    for x in temp1:
+        if x not in ind1:
+            ind1.append(x)
+    ind2 = []
+    for x in temp2:
+        if x not in ind2:
+            ind2.append(x)
+    return ind1, ind2
 
-def gaVRPTW(individual, instance, indSize, popSize):
+def mutInverseIndexes(individual):
+    start, stop = sorted(random.sample(range(len(individual)), 2))
+    individual = individual[:start] + individual[stop:start-1:-1] + individual[stop+1:]
+    return individual,
+
+def gaVRPTW(individual, instance, indSize, popSize, cxPb, mutPb, nGen):
     creator.create('FitnessMax', base.Fitness, weights=(1.0,))
     creator.create('Individual', list, fitness=creator.FitnessMax)
     toolbox = base.Toolbox()
@@ -24,8 +43,36 @@ def gaVRPTW(individual, instance, indSize, popSize):
     toolbox.register('individual', tools.initIterate, creator.Individual, toolbox.indexes)
     toolbox.register('population', tools.initRepeat, list, toolbox.individual)
     # operator registering
-    toolbox.register('evaluate', evalVRPTW, instance=instance, )
-    
+    toolbox.register('evaluate', evalVRPTW, instance=instance)
+    toolbox.register('select', tools.selRoulette)
+    toolbox.register('mate', cxPartiallyMatched)
+    toolbox.register('mutate', mutInverseIndexes)
+    pop = toolbox.population(n=popSize)
+    # evaluate the entire population
+    fitnesses = list(map(toolbox.evaluate, pop))
+    for ind, fit in zip(pop, fitnesses):
+        ind.fitness.values = fit
+    # begin evolution
+    for g in range(nGen):
+        offspring = toolbox.select(pop, len(pop))
+        offspring = list(map(toolbox.clone, offspring))
+        for child1, child2 in zip(offspring[::2], offspring[1::2]):
+            if random.random() < cxPb:
+                toolbox.mate(child1, child2)
+                del child1.fitness.values
+                del child2.fitness.values
+        for mutant in offspring:
+            if random.random() < mutPb:
+                toolbox.mutate(mutant)
+                del mutant.fitness.values
+        # evaluate individuals with an invalid fitness
+        invalidInd = [ind for ind in offspring if not ind.fitness.valid]
+        fitnesses = map(toolbox.evaluate, invalidInd)
+        for ind, fit in zip(invalidInd, fitnesses):
+            ind.fitness.values = fit
+        pop[:] = offspring
+        bestInd = tools.selBest(pop, 1)[0]
+        ind2route(bestInd, instance)
 
 class Ride(types.SimpleNamespace):
     def __init__(self, ride_number, start_x, start_y, end_x, end_y, start_time,
@@ -81,7 +128,6 @@ def main():
 def solve(rides):
     s = Solution()
     rides.sort(key=lambda r: r.start_time)
-
     vehicle_number = 0
     for ride in rides:
         s.assign(vehicle_number, ride_number=ride.ride_number)
